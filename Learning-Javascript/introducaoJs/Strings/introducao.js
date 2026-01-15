c6c03397ea9f9a047647ecc74e81a65a8f130ea3
Foqsz <contatovictorvinicius05@gmail.com>
2026-01-15 09:28:40 -0300

    feat: slice e substring

---
+++ Learning-Javascript/introducaoJs/Strings/introducao.js
@@ -0,0 +1,8 @@
+let str1 = "Hello, World!";
+console.log(str1.replace("World", "JavaScript")); // "Hello, JavaScript!"
+
+console.log(str1.indexOf("World"));
+console.log(str1.includes("World"))
+
+console.log(str1.slice(7, 12)); // "World"
+console.log(str1.substring(7, 12)); // "World"
