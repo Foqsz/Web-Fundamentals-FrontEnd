2ceb7520b8a9a3785fae6e5937f60943fbca01b7
Foqsz <contatovictorvinicius05@gmail.com>
2026-01-15 09:56:09 -0300

    feat: toUpperCase, LowerCase, Valueof, toString, trim, trimStart e trimEnd

--- Learning-Javascript/introducaoJs/Strings/introducao.js
+++ Learning-Javascript/introducaoJs/Strings/introducao.js
@@ -6,3 +6,16 @@ console.log(str1.includes("World"))
 
 console.log(str1.slice(7, 12)); // "World"
 console.log(str1.substring(7, 12)); // "World"
+
+console.log(str1.toUpperCase()); // "HELLO, WORLD!"
+console.log(str1.toLowerCase()); // "hello, world!"
+
+let strAsObj = new String("minha string como objeto");
+console.log(typeof strAsObj);
+console.log(strAsObj.valueOf());
+console.log(strAsObj.toString());
+
+let str2 = "   Espaços em branco   ";
+console.log(str2.trim()); // "Espaços em branco"
+console.log(str2.trimStart()); // "Espaços em branco   "
+console.log(str2.trimEnd()); // "   Espaços em branco"
